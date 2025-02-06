dea2d9077d9c6f2d4c3576c6adbaa8f7e3836131
harintin <harintin@github.com>
2025-02-06 22:47:35 +0900

    js fix

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -26,90 +26,6 @@ gsap.from(".line", {
   },
 });
 
-// slide js
-
-document.addEventListener("DOMContentLoaded", function () {
-  var swiper = new Swiper(".mySwiper", {});
-
-  var isSnapped = true;
-
-  ScrollTrigger.create({
-    trigger: "#swiper-section",
-    start: "top top",
-    end: "bottom bottom",
-    onEnter: () => {
-      var wrapper = document.querySelector(".swiper-wrapper");
-      var trigger = document.querySelector("#swiper-section");
-      var centerY = (trigger.clientHeight - wrapper.clientHeight) / 2;
-      gsap.to(wrapper, {
-        y: centerY,
-        duration: 0.5,
-        ease: "power2.inOut",
-        onComplete: () => {
-          isSnapped = false;
-        },
-      });
-    },
-    onLeaveBack: () => {
-      var wrapper = document.querySelector(".swiper-wrapper");
-      if (isSnapped) {
-        var trigger = document.querySelector("#swiper-section");
-        var centerY = (trigger.clientHeight - wrapper.clientHeight) / 2;
-        gsap.to(wrapper, {
-          y: centerY,
-          duration: 0.2,
-          ease: "power2.inOut",
-          onComplete: () => {
-            isSnapped = false;
-          },
-        });
-      }
-    },
-  });
-
-  swiper.on("slideChange", function () {
-    if (swiper.isEnd) {
-      ScrollTrigger.getById("swiper-section").disable();
-    } else {
-      ScrollTrigger.getById("swiper-section").enable();
-    }
-  });
-});
-
-// scroll smooth
-
-gsap.registerPlugin(ScrollTrigger);
-
-const sections = document.querySelectorAll("#sectionAbout");
-
-sections.forEach((section, index) => {
-  ScrollTrigger.create({
-    trigger: "#main",
-    start: "top top",
-    end: "bottom top",
-    pin: true,
-    pinSpacing: false,
-    onEnter: () => console.log(`Entered section ${index + 1}`),
-    onLeaveBack: () => console.log(`Left section ${index + 1}`),
-  });
-});
-
-gsap.registerPlugin(ScrollTrigger);
-
-const slide = document.querySelectorAll("#swiper-section");
-
-sections.forEach((section, index) => {
-  ScrollTrigger.create({
-    trigger: "#slide-tit",
-    start: "top top",
-    end: "bottom top",
-    pin: true,
-    pinSpacing: false,
-    onEnter: () => console.log(`Entered section ${index + 1}`),
-    onLeaveBack: () => console.log(`Left section ${index + 1}`),
-  });
-});
-
 // cursor
 
 document.addEventListener("DOMContentLoaded", function (event) {
@@ -167,18 +83,4 @@ window.addEventListener("scroll", function () {
 
 button.addEventListener("click", function () {
   document.documentElement.scrollTop = 0;
-});
-
-// smooth
-
-function smoothScrollTo(yPosition) {
-  gsap.to(window, {
-    duration: 100,
-    scrollTo: yPosition,
-    ease: "power4.inOut",
-  });
-}
-
-document.getElementById("scrollButton").addEventListener("click", function () {
-  smoothScrollTo(2000);
 });
